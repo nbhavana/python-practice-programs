488eca6717fc55cfccf9a0f85e7e5f8016714b65
Kunal Nawale <nkunal@gmail.com>
2014-01-04 16:08:33 -0500

    python pratice programs

---
+++ addition.py
@@ -0,0 +1,21 @@
+
+"""
+Program to calculate sum of two numbers
+
+"""
+
+def add(a,b):
+    print a
+    print b
+
+    print int(a)+int(b)
+    
+
+print ("Enter value for a:")
+a=raw_input()
+
+
+print ("Enter value for b:")
+b=raw_input()
+
+add(a,b)
